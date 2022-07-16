46f8ab5828bbeb193ad7d298c0330611f6b929e0
Mamoun2020 <mmun2019@gmail.com>
2022-07-16 07:57:47 +0300

    Initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return TaskResource::collection(Task::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTaskRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTaskRequest  $request
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        //
+    }
+}
---
+++ tests/Feature/TaskControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+
+use Database\Factories\TaskFactory;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TaskControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_can_get_all_tasks(){
+        //Arrange
+        $tasks = TaskFactory::new()->count(20)->create();
+        //Art
+        $response =  $this->getJson('api/tasks');
+        //Assert
+        $response->dump() ->assertJsonStructure(
+          [
+              'data' =>[
+                [
+                    'id',
+                    'title',
+                    'status'
+                ],
+              ],
+          ]
+        );
+    }
+
+}
